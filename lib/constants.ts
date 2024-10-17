91913bf1753efa0a95c730d103f969ba40dfade7
AryanMahajan <mahajanaryan0409@gmail.com>
2024-10-17 06:43:06 +0530

    new_certificate_added

--- lib/constants.ts
+++ lib/constants.ts
@@ -19,9 +19,19 @@ export const Certificates = [
         title: "Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning",
         imageSrc: "/deeplearning.png"
     },
+    {
+        title: "Advance Learning Algorithms",
+        imageSrc: "/deeplearning2.png"
+    },
 ]
 
 export const MyProjects = [
+    {
+        title: "Lunar Lander with NEAT",
+        description: "This project is an AI-powered Lunar Lander simulation using NEAT (NeuroEvolution of Augmenting Topologies) and Pygame. The AI tries to controls the lander to perform smooth landings on a platform while avoiding crashes, learning from each generation using evolutionary algorithms.",
+        imageSrc: "/medium.png",
+        link: "https://github.com/AryanMahajan/Lunar_Lander_with_NEAT"
+    },
     {
         title: "Flappy Bird with NEAT",
         description: "NeuroEvolution of Augmenting Topologies (NEAT) is a genetic algorithm (GA) for evolving artificial neural networks. This project showcases NEAT's ability to solve complex tasks like playing Flappy Bird by merging neural network flexibility with genetic algorithm efficiency. It's an excellent example of AI in game development.",
@@ -33,12 +43,6 @@ export const MyProjects = [
         description: "DOTS is a machine learning model which can classify whether the image is of a dog or a cat using CNN method and is made using TensorFlow.",
         imageSrc: "/vice-website.png",
         link: "https://github.com/AryanMahajan/DOTS"
-    },
-    {
-        title: "House Price Prediction using TensorFlow and Keras",
-        description: "This project predicts house prices using three features: area, number of bedrooms, and bathrooms. It leverages TensorFlow and Keras to build and train the machine learning model.",
-        imageSrc: "/medium.png",
-        link: "https://github.com/AryanMahajan/House_Price_Prediction_Model"
     },
     {
         title: "House Price Prediction using TensorFlow and Keras",
