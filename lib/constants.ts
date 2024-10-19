4cb119d60da258ef2e76fc6f1453bcdfee77dd8f
AryanMahajan <mahajanaryan0409@gmail.com>
2024-10-19 16:05:30 +0530

    project_images_added

--- lib/constants.ts
+++ lib/constants.ts
@@ -29,13 +29,13 @@ export const MyProjects = [
     {
         title: "Lunar Lander with NEAT",
         description: "This project is an AI-powered Lunar Lander simulation using NEAT (NeuroEvolution of Augmenting Topologies) and Pygame. The AI tries to controls the lander to perform smooth landings on a platform while avoiding crashes, learning from each generation using evolutionary algorithms.",
-        imageSrc: "/medium.png",
+        imageSrc: "/lunar_lander.png",
         link: "https://github.com/AryanMahajan/Lunar_Lander_with_NEAT"
     },
     {
         title: "Flappy Bird with NEAT",
         description: "NeuroEvolution of Augmenting Topologies (NEAT) is a genetic algorithm (GA) for evolving artificial neural networks. This project showcases NEAT's ability to solve complex tasks like playing Flappy Bird by merging neural network flexibility with genetic algorithm efficiency. It's an excellent example of AI in game development.",
-        imageSrc: "/medium.png",
+        imageSrc: "/flappy_bird.png",
         link: "https://github.com/AryanMahajan/Flappy_Bird_With_NEAT"
     },
     {
@@ -47,13 +47,13 @@ export const MyProjects = [
     {
         title: "Dog or Cat Sorter (DOTS)",
         description: "DOTS is a machine learning model which can classify whether the image is of a dog or a cat using CNN method and is made using TensorFlow.",
-        imageSrc: "/vice-website.png",
+        imageSrc: "/DOTS.png",
         link: "https://github.com/AryanMahajan/DOTS"
     },
     {
         title: "Industrial_Machine_Fault_Detection",
         description: "DOTS is a machine learning model which can classify whether the image is of a dog or a cat using CNN method and is made using TensorFlow.",
-        imageSrc: "/vice-website.png",
+        imageSrc: "/fault.png",
         link: "https://github.com/AryanMahajan/Industrial_Machine_Fault_Detection"
     },
 ]
